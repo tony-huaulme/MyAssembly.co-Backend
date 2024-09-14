e11cb651bcd6dd353d15e1bac40c4c2d15872271
tony-huaulme <tonyhuaulme@gmail.com>
2024-09-14 22:48:38 +0200

    postgreSQL connection

--- app.py
+++ app.py
@@ -80,6 +80,25 @@ def handle_file(filename):
         except NoCredentialsError:
             return jsonify({'error': 'Credentials not available'}), 403
 
+# Get all users
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    users_list = [{'id': user.id, 'username': user.username, 'email': user.email} for user in users]
+    return jsonify(users_list)
+
+# Add a new user
+@app.route('/users', methods=['POST'])
+def add_user():
+    data = request.json
+    username = data.get('username')
+    email = data.get('email')
+    if not username or not email:
+        return jsonify({'error': 'Missing username or email'}), 400
+    new_user = User(username=username, email=email)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'User added successfully', 'user': {'id': new_user.id, 'username': new_user.username, 'email': new_user.email}}), 201
 
 
 # Create the tables (if they don’t exist already)
