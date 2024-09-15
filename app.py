3cffa24d08c19c3750672208361e4a727a14411b
tony-huaulme <tonyhuaulme@gmail.com>
2024-09-15 10:01:47 +0200

    new setup

--- app.py
+++ app.py
@@ -1,136 +1,141 @@
-# from flask import Flask, request, send_file, jsonify
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_dance.contrib.google import make_google_blueprint, google
-# from models import db, User
-# from config import Config
-# import boto3
-# from botocore.exceptions import NoCredentialsError, ClientError
-# from io import BytesIO
-# from auth import auth_bp  # Import the auth_bp blueprint from auth.py
-
-# app = Flask(__name__)
-# app.config.from_object(Config)
-
-# # Initialize extensions
-# db.init_app(app)
-
-# # Register the auth blueprint
-# app.register_blueprint(auth_bp)  # Registering the auth_bp blueprint
-
-# # Google OAuth blueprint
-# google_bp = make_google_blueprint(
-#     client_id=app.config['GOOGLE_OAUTH_CLIENT_ID'],
-#     client_secret=app.config['GOOGLE_OAUTH_CLIENT_SECRET'],
-#     redirect_to='auth.google_authorized',
-#     scope=['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email'] 
-# )
-# app.register_blueprint(google_bp, url_prefix='/login')
-
-# # Initialize S3 client
-# s3 = boto3.client('s3',
-#                   aws_access_key_id=app.config['AWS_ACCESS'],
-#                   aws_secret_access_key=app.config['AWS_SECRET'],
-#                   region_name=app.config['AWS_REGION'])
-
-# @app.route('/')
-# def index():
-#     return "Welcome to the Flask App with Google OAuth! <a href='/login'>Login</a>"
-
-
-# #Handle files
-# @app.route('/files/<path:filename>', methods=['GET', 'POST', 'DELETE'])
-# def handle_file(filename):
-#     if request.method == 'GET':
-#         # Download file
-#         try:
-#             response = s3.get_object(Bucket=app.config['AWS_BUCKET_NAME'], Key=filename)
-#             file_stream = BytesIO(response['Body'].read())
-#             return send_file(file_stream, as_attachment=True, download_name=filename.split('/')[-1])
-#         except ClientError as e:
-#             if e.response['Error']['Code'] == 'NoSuchKey':
-#                 return jsonify({'error': 'File not found'}), 404
-#             else:
-#                 return jsonify({'error': str(e)}), 500
-#         except NoCredentialsError:
-#             return jsonify({'error': 'Credentials not available'}), 403
-
-#     elif request.method == 'POST':
-#         # Upload file
-#         file = request.files.get('file')
-#         if file:
-#             try:
-#                 s3.upload_fileobj(file, app.config['AWS_BUCKET_NAME'], filename)
-#                 return jsonify({'message': 'File uploaded successfully'}), 201
-#             except NoCredentialsError:
-#                 return jsonify({'error': 'Credentials not available'}), 403
-#             except ClientError as e:
-#                 return jsonify({'error': str(e)}), 500
-#         else:
-#             return jsonify({'error': 'No file provided'}), 400
-
-#     elif request.method == 'DELETE':
-#         # Delete file
-#         try:
-#             s3.delete_object(Bucket=app.config['AWS_BUCKET_NAME'], Key=filename)
-#             return jsonify({'message': 'File deleted successfully'}), 200
-#         except ClientError as e:
-#             if e.response['Error']['Code'] == 'NoSuchKey':
-#                 return jsonify({'error': 'File not found'}), 404
-#             else:
-#                 return jsonify({'error': str(e)}), 500
-#         except NoCredentialsError:
-#             return jsonify({'error': 'Credentials not available'}), 403
-
-# # Get all users
-# @app.route('/users', methods=['GET'])
-# def get_users():
-#     users = User.query.all()
-#     users_list = [{'id': user.id, 'username': user.username, 'email': user.email} for user in users]
-#     return jsonify(users_list)
-
-# # Add a new user
-# @app.route('/users', methods=['POST'])
-# def add_user():
-#     data = request.json
-#     username = data.get('username')
-#     email = data.get('email')
-#     if not username or not email:
-#         return jsonify({'error': 'Missing username or email'}), 400
-#     new_user = User(username=username, email=email)
-#     db.session.add(new_user)
-#     db.session.commit()
-#     return jsonify({'message': 'User added successfully', 'user': {'id': new_user.id, 'username': new_user.username, 'email': new_user.email}}), 201
-
-
-# # Create the tables (if they don’t exist already)
-# with app.app_context():
-#     db.create_all()
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-from flask import Flask, redirect, url_for
-from flask_dance.contrib.google import make_google_blueprint, google
+import json
+import requests
+from flask import Flask, redirect, request
+from oauthlib import oauth2
+from flask_sqlalchemy import SQLAlchemy
+from models import db, User
 from config import Config
-
+from io import BytesIO
+import boto3
+from botocore.exceptions import NoCredentialsError, ClientError
+from flask import send_file, jsonify
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-google_bp = make_google_blueprint(
-    client_id=app.config['GOOGLE_OAUTH_CLIENT_ID'],
-    client_secret=app.config['GOOGLE_OAUTH_CLIENT_SECRET'],
-    redirect_to='google_login'
+# Google OAuth2 configuration
+CLIENT_ID = app.config['GOOGLE_OAUTH_CLIENT_ID']
+CLIENT_SECRET = app.config['GOOGLE_OAUTH_CLIENT_SECRET']
+
+
+
+DATA = {
+    'response_type': "code",
+    'redirect_uri': "https://127.0.0.1:5000/home",
+    'scope': 'https://www.googleapis.com/auth/userinfo.email',
+    'client_id': CLIENT_ID,
+    'prompt': 'consent'
+}
+URL_DICT = {
+    'google_oauth': 'https://accounts.google.com/o/oauth2/v2/auth',
+    'token_gen': 'https://oauth2.googleapis.com/token',
+    'get_user_info': 'https://www.googleapis.com/oauth2/v3/userinfo'
+}
+CLIENT = oauth2.WebApplicationClient(CLIENT_ID)
+REQ_URI = CLIENT.prepare_request_uri(
+    uri=URL_DICT['google_oauth'],
+    redirect_uri=DATA['redirect_uri'],
+    scope=DATA['scope'],
+    prompt=DATA['prompt']
 )
-app.register_blueprint(google_bp, url_prefix='/google_login')
+
+
+# Initialize extensions
+db.init_app(app)
+
+# Initialize S3 client
+s3 = boto3.client('s3',
+                  aws_access_key_id=app.config['AWS_ACCESS'],
+                  aws_secret_access_key=app.config['AWS_SECRET'],
+                  region_name=app.config['AWS_REGION'])
 
 @app.route('/')
-def home():
-    if not google.authorized:
-        return redirect(url_for('google.login'))
-    resp = google.get('/plus/v1/people/me')
-    assert resp.ok, resp.text
-    return 'You are connected with Google as: {0}'.format(resp.json()['displayName'])
+def login():
+    return redirect(REQ_URI)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/home')
+def home():
+    code = request.args.get('code')
+    token_url, headers, body = CLIENT.prepare_token_request(
+        URL_DICT['token_gen'],
+        authorization_response=request.url,
+        redirect_url=request.base_url,
+        code=code
+    )
+    token_response = requests.post(
+        token_url,
+        headers=headers,
+        data=body,
+        auth=(CLIENT_ID, CLIENT_SECRET)
+    )
+    CLIENT.parse_request_body_response(json.dumps(token_response.json()))
+    uri, headers, body = CLIENT.add_token(URL_DICT['get_user_info'])
+    response_user_info = requests.get(uri, headers=headers, data=body)
+    info = response_user_info.json()
+    return redirect('/user/%s' % info['email'])
+
+@app.route('/user/<email>')
+def login_success(email):
+    return "Hello %s" % email
+
+@app.route('/files/<path:filename>', methods=['GET', 'POST', 'DELETE'])
+def handle_file(filename):
+    if request.method == 'GET':
+        try:
+            response = s3.get_object(Bucket=app.config['AWS_BUCKET_NAME'], Key=filename)
+            file_stream = BytesIO(response['Body'].read())
+            return send_file(file_stream, as_attachment=True, download_name=filename.split('/')[-1])
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'NoSuchKey':
+                return jsonify({'error': 'File not found'}), 404
+            else:
+                return jsonify({'error': str(e)}), 500
+        except NoCredentialsError:
+            return jsonify({'error': 'Credentials not available'}), 403
+    elif request.method == 'POST':
+        file = request.files.get('file')
+        if file:
+            try:
+                s3.upload_fileobj(file, app.config['AWS_BUCKET_NAME'], filename)
+                return jsonify({'message': 'File uploaded successfully'}), 201
+            except NoCredentialsError:
+                return jsonify({'error': 'Credentials not available'}), 403
+            except ClientError as e:
+                return jsonify({'error': str(e)}), 500
+        else:
+            return jsonify({'error': 'No file provided'}), 400
+    elif request.method == 'DELETE':
+        try:
+            s3.delete_object(Bucket=app.config['AWS_BUCKET_NAME'], Key=filename)
+            return jsonify({'message': 'File deleted successfully'}), 200
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'NoSuchKey':
+                return jsonify({'error': 'File not found'}), 404
+            else:
+                return jsonify({'error': str(e)}), 500
+        except NoCredentialsError:
+            return jsonify({'error': 'Credentials not available'}), 403
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    users_list = [{'id': user.id, 'username': user.username, 'email': user.email} for user in users]
+    return jsonify(users_list)
+
+@app.route('/users', methods=['POST'])
+def add_user():
+    data = request.json
+    username = data.get('username')
+    email = data.get('email')
+    if not username or not email:
+        return jsonify({'error': 'Missing username or email'}), 400
+    new_user = User(username=username, email=email)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'User added successfully', 'user': {'id': new_user.id, 'username': new_user.username, 'email': new_user.email}}), 201
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000, ssl_context='adhoc')
--- auth.py
+++ auth.py
@@ -1,44 +1,57 @@
-from flask import Blueprint, redirect, url_for, session
-from flask_dance.contrib.google import google
+from flask import Blueprint, redirect, url_for, session, request
+from oauthlib.oauth2 import WebApplicationClient
+from requests_oauthlib import OAuth2Session
 from models import db, User
+import os
 
 auth_bp = Blueprint('auth', __name__)
 
+# Google OAuth2 configuration
+CLIENT_ID = os.getenv('GOOGLE_OAUTH_CLIENT_ID')
+CLIENT_SECRET = os.getenv('GOOGLE_OAUTH_CLIENT_SECRET')
+AUTHORIZATION_BASE_URL = 'https://accounts.google.com/o/oauth2/v2/auth'
+TOKEN_URL = 'https://oauth2.googleapis.com/token'
+USER_INFO_URL = 'https://www.googleapis.com/oauth2/v3/userinfo'
+
+# Create a client instance
+client = WebApplicationClient(CLIENT_ID)
+
 @auth_bp.route('/login')
 def login():
-    if not google.authorized:
-        return redirect(url_for('google.login'))
-    
-    resp = google.get('/oauth2/v2/userinfo')
-    assert resp.ok, resp.text
-    user_info = resp.json()
-    print("\n\nUser Info\n\n")
-    print(user_info)
-    print("\n\n....\n\n")
-    
-    # Get user from DB or create a new one
-    user = User.query.filter_by(email=user_info.get("email")).first()
-    if not user:
-        user = User(username=user_info.get("name"), email=user_info.get("email"), google_id=user_info.get("id"))
-        db.session.add(user)
-        db.session.commit()
-    
-    session['user'] = user_info.get("name")
-    return f'Logged in as {session["user"]}'
+    authorization_url, state = client.authorization_url(
+        AUTHORIZATION_BASE_URL,
+        access_type='offline',
+        prompt='consent',
+        redirect_uri=url_for('auth.google_authorized', _external=True)
+    )
+    session['oauth_state'] = state
+    return redirect(authorization_url)
 
 @auth_bp.route('/login/google/authorized')
 def google_authorized():
-    if not google.authorized:
-        return redirect(url_for('google.login'))
+    # Get authorization response
+    code = request.args.get('code')
+    token_url, headers, body = client.prepare_token_request(
+        TOKEN_URL,
+        authorization_response=request.url,
+        redirect_url=url_for('auth.google_authorized', _external=True),
+        code=code
+    )
     
-    resp = google.get('/oauth2/v2/userinfo')
-    assert resp.ok, resp.text
-    user_info = resp.json()
+    # Fetch token
+    oauth_session = OAuth2Session(CLIENT_ID, client_secret=CLIENT_SECRET)
+    token_response = oauth_session.post(token_url, headers=headers, data=body)
+    token_json = token_response.json()
+    client.parse_request_body_response(token_response.text)
     
-    # Process user information and update session
+    # Get user info
+    oauth_session = OAuth2Session(CLIENT_ID, token=token_json)
+    user_info_response = oauth_session.get(USER_INFO_URL)
+    user_info = user_info_response.json()
+
     user = User.query.filter_by(email=user_info.get("email")).first()
     if not user:
-        user = User(username=user_info.get("name"), email=user_info.get("email"), google_id=user_info.get("id"))
+        user = User(username=user_info.get("name"), email=user_info.get("email"), google_id=user_info.get("sub"))
         db.session.add(user)
         db.session.commit()
     
