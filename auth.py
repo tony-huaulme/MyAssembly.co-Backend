77628e590c56f153534af5885dc49c127692e853
tony-huaulme <tonyhuaulme@gmail.com>
2024-09-15 07:01:24 +0200

    handel googleauth userinfo

--- auth.py
+++ auth.py
@@ -12,7 +12,9 @@ def login():
     resp = google.get('/oauth2/v1/userinfo')
     assert resp.ok, resp.text
     user_info = resp.json()
-    
+    print("\n\nUesr Info\n\n")
+    print(user_info)
+    print("\n\n....\n\n")
     # Get user from DB or create a new one
     user = User.query.filter_by(email=user_info["email"]).first()
     if not user:
@@ -22,6 +24,25 @@ def login():
     
     session['user'] = user_info["name"]
     return f'Logged in as {session["user"]}'
+
+@auth_bp.route('/login/google/authorized')
+def google_authorized():
+    if not google.authorized:
+        return redirect(url_for('google.login'))
+    resp = google.get('/oauth2/v1/userinfo')
+    assert resp.ok, resp.text
+    user_info = resp.json()
+    
+    # Process user information and update session
+    user = User.query.filter_by(email=user_info["email"]).first()
+    if not user:
+        user = User(username=user_info["name"], email=user_info["email"], google_id=user_info["id"])
+        db.session.add(user)
+        db.session.commit()
+    
+    session['user'] = user_info["name"]
+    return f'Logged in as {session["user"]}'
+
 
 @auth_bp.route('/logout')
 def logout():
