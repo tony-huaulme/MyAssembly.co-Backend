ad5317c4761ed0d20948cc9be8709389c8520a67
tony-huaulme <tonyhuaulme@gmail.com>
2024-09-15 07:15:54 +0200

    missig key handle

--- auth.py
+++ auth.py
@@ -12,37 +12,38 @@ def login():
     resp = google.get('/oauth2/v1/userinfo')
     assert resp.ok, resp.text
     user_info = resp.json()
-    print("\n\nUesr Info\n\n")
+    print("\n\nUser Info\n\n")
     print(user_info)
     print("\n\n....\n\n")
+    
     # Get user from DB or create a new one
-    user = User.query.filter_by(email=user_info["email"]).first()
+    user = User.query.filter_by(email=user_info.get("email")).first()
     if not user:
-        user = User(username=user_info["name"], email=user_info["email"], google_id=user_info["id"])
+        user = User(username=user_info.get("name"), email=user_info.get("email"), google_id=user_info.get("id"))
         db.session.add(user)
         db.session.commit()
     
-    session['user'] = user_info["name"]
+    session['user'] = user_info.get("name")
     return f'Logged in as {session["user"]}'
 
 @auth_bp.route('/login/google/authorized')
 def google_authorized():
     if not google.authorized:
         return redirect(url_for('google.login'))
+    
     resp = google.get('/oauth2/v1/userinfo')
     assert resp.ok, resp.text
     user_info = resp.json()
     
     # Process user information and update session
-    user = User.query.filter_by(email=user_info["email"]).first()
+    user = User.query.filter_by(email=user_info.get("email")).first()
     if not user:
-        user = User(username=user_info["name"], email=user_info["email"], google_id=user_info["id"])
+        user = User(username=user_info.get("name"), email=user_info.get("email"), google_id=user_info.get("id"))
         db.session.add(user)
         db.session.commit()
     
-    session['user'] = user_info["name"]
+    session['user'] = user_info.get("name")
     return f'Logged in as {session["user"]}'
-
 
 @auth_bp.route('/logout')
 def logout():
