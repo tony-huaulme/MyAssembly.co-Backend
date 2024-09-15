8bd82a0bf1c1ff08ac29105cccf2cacbcdeb08eb
tony-huaulme <tonyhuaulme@gmail.com>
2024-09-15 10:28:45 +0200

    working

--- app.py
+++ app.py
@@ -1,27 +1,16 @@
 from flask import Flask, redirect, url_for
-from flask_dance.contrib.google import make_google_blueprint, google
 from config import Config
+from auth import google_blueprint, index
 
 app = Flask(__name__)
-
 app.config.from_object(Config)
-
-
 app.secret_key = app.config['SECRET_KEY']
-blueprint = make_google_blueprint(
-    client_id=app.config['GOOGLE_CLIENT_ID'],
-    client_secret=app.config['GOOGLE_CLIENT_SECRET'],
-    scope=["profile", "email"]
-)
-app.register_blueprint(blueprint, url_prefix="/login")
+
+app.register_blueprint(google_blueprint, url_prefix="/login")
 
 @app.route("/")
-def index():
-    if not google.authorized:
-        return redirect(url_for("google.login"))
-    resp = google.get("/plus/v1/people/me")
-    assert resp.ok, resp.text
-    return "You are {email} on Google".format(email=resp.json()["emails"][0]["value"])
+def home():
+    return index()
 
 if __name__ == "__main__":
     app.run()
--- auth.py
+++ auth.py
@@ -1,64 +1,18 @@
-from flask import Blueprint, redirect, url_for, session, request
-from oauthlib.oauth2 import WebApplicationClient
-from requests_oauthlib import OAuth2Session
-from models import db, User
-import os
+from flask import redirect, url_for
+from flask_dance.contrib.google import make_google_blueprint, google
+from config import Config
 
-auth_bp = Blueprint('auth', __name__)
+blueprint = make_google_blueprint(
+    client_id=Config.GOOGLE_CLIENT_ID,
+    client_secret=Config.GOOGLE_CLIENT_SECRET,
+    scope=["profile", "email"]
+)
 
-# Google OAuth2 configuration
-CLIENT_ID = os.getenv('GOOGLE_OAUTH_CLIENT_ID')
-CLIENT_SECRET = os.getenv('GOOGLE_OAUTH_CLIENT_SECRET')
-AUTHORIZATION_BASE_URL = 'https://accounts.google.com/o/oauth2/v2/auth'
-TOKEN_URL = 'https://oauth2.googleapis.com/token'
-USER_INFO_URL = 'https://www.googleapis.com/oauth2/v3/userinfo'
+def index():
+    if not google.authorized:
+        return redirect(url_for("google.login"))
+    resp = google.get("/plus/v1/people/me")
+    assert resp.ok, resp.text
+    return "You are {email} on Google".format(email=resp.json()["emails"][0]["value"])
 
-# Create a client instance
-client = WebApplicationClient(CLIENT_ID)
-
-@auth_bp.route('/login')
-def login():
-    authorization_url, state = client.authorization_url(
-        AUTHORIZATION_BASE_URL,
-        access_type='offline',
-        prompt='consent',
-        redirect_uri=url_for('auth.google_authorized', _external=True)
-    )
-    session['oauth_state'] = state
-    return redirect(authorization_url)
-
-@auth_bp.route('/login/google/authorized')
-def google_authorized():
-    # Get authorization response
-    code = request.args.get('code')
-    token_url, headers, body = client.prepare_token_request(
-        TOKEN_URL,
-        authorization_response=request.url,
-        redirect_url=url_for('auth.google_authorized', _external=True),
-        code=code
-    )
-    
-    # Fetch token
-    oauth_session = OAuth2Session(CLIENT_ID, client_secret=CLIENT_SECRET)
-    token_response = oauth_session.post(token_url, headers=headers, data=body)
-    token_json = token_response.json()
-    client.parse_request_body_response(token_response.text)
-    
-    # Get user info
-    oauth_session = OAuth2Session(CLIENT_ID, token=token_json)
-    user_info_response = oauth_session.get(USER_INFO_URL)
-    user_info = user_info_response.json()
-
-    user = User.query.filter_by(email=user_info.get("email")).first()
-    if not user:
-        user = User(username=user_info.get("name"), email=user_info.get("email"), google_id=user_info.get("sub"))
-        db.session.add(user)
-        db.session.commit()
-    
-    session['user'] = user_info.get("name")
-    return f'Logged in as {session["user"]}'
-
-@auth_bp.route('/logout')
-def logout():
-    session.pop('user', None)
-    return redirect(url_for('index'))
+google_blueprint = blueprint
